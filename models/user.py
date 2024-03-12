1bdaa30418a5de4618d1bf4995c66734356285f3
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-12 21:20:26 +0100

    update: add database migration

--- models/user.py
+++ models/user.py
@@ -8,6 +8,7 @@ Attributes:
     - first_name (str): User first name.
     - last_name (float): User last name.
     - email (str): User email address.
+    - username(str): User username
     - password (str): User password
     - phone_no (str) : User phone number
     - is_admin (boolean) : specifies if user is an admin or just regular user
@@ -15,7 +16,7 @@ Attributes:
 
 from hashlib import md5
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Float, Integer, Boolean
+from sqlalchemy import Column, String, Boolean
 from models.json_dict import JsonEncodedDict, default_measurement
 
 
@@ -25,6 +26,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    username = Column(String(128), nullable=False)
     email = Column(String(1024), nullable=False)
     password = Column(String(1024), nullable=False)
     phone_no = Column(String(128), nullable=True)
